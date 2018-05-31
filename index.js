0e625ef6ed113d08d42a18ede6a81ff0869c5361
hzchenjinghui <hzchenjinghui@corp.netease.com>
2018-05-31 16:21:25 +0800

    add js support

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const fs = require('fs')
 const stripJsonComments = require('strip-json-comments')
 
 module.exports = options => (req, res, next) => {
+    const jsFilePath = path.resolve(options.root, `./${req.method.toLowerCase()}`, `./${req.path}`, 'data.js')
+    if(!fs.existsSync(jsFilePath)){
+        console.log('data from js')
+        return res.status(200).json(require(jsFilePath)(req))
+    }
+
     const filePath = path.resolve(options.root, `./${req.method.toLowerCase()}`, `./${req.path}`, 'data.json')
     
     if(!fs.existsSync(filePath)) return next()
