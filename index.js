ce91737e6fe1e4eacf0bae1481a4a42b34abd6fd
hzchenjinghui <hzchenjinghui@corp.netease.com>
2018-05-31 16:21:58 +0800

    rm console.log

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const stripJsonComments = require('strip-json-comments')
 module.exports = options => (req, res, next) => {
     const jsFilePath = path.resolve(options.root, `./${req.method.toLowerCase()}`, `./${req.path}`, 'data.js')
     if(!fs.existsSync(jsFilePath)){
-        console.log('data from js')
         return res.status(200).json(require(jsFilePath)(req))
     }
 
